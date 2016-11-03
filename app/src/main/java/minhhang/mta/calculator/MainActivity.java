99443e23f7d66f789e670560c044a2790918086d
MinhHang <minhhang.mta@gmail.com>
2016-11-04 03:38:39 +0700

    Load lan 1: finish layout

---
+++ app/src/main/java/minhhang/mta/calculator/MainActivity.java
@@ -0,0 +1,23 @@
+package minhhang.mta.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //TextView txtV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Anh xa:
+        //txtV=(TextView) findViewById(R.id.txtEx);
+
+        //Viet code:
+        ////
+        //txtV.setText("Hello World");
+
+    }
+}
